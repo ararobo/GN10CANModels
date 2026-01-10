8062e3f1af524bc3bd0c5fc53987a5d510e92fca
gn10g <8gn24gn25@gmail.com>
2026-01-10 19:49:04 +0900

    Enhance can_converter.hpp documentation with detailed descriptions for functions

--- include/gn10_can/utils/can_converter.hpp
+++ include/gn10_can/utils/can_converter.hpp
@@ -1,3 +1,13 @@
+/**
+ * @file can_converter.hpp
+ * @author Gento Aiba (aiba-gento)
+ * @brief CANデータ変換ユーティリティヘッダーファイル
+ * @version 0.1
+ * @date 2026-01-10
+ *
+ * @copyright Copyright (c) 2026 Gento Aiba
+ * SPDX-License-Identifier: GPL-3.0
+ */
 #pragma once
 
 #include <cstdint>
@@ -9,6 +19,17 @@
 namespace gn10_can {
 namespace converter {
 
+/**
+ * @brief バッファにPOD型データを収納する関数
+ *
+ * @tparam T 収納するPOD型データの型
+ * @param buffer データを収納するバッファ
+ * @param buffer_len バッファの長さ
+ * @param start_byte 開始バイト位置
+ * @param value 収納するPOD型データ
+ * @return true 成功
+ * @return false 失敗（バッファオーバーフローなど）
+ */
 template <typename T>
 bool pack(uint8_t* buffer, size_t buffer_len, uint8_t start_byte, T value) {
     static_assert(std::is_trivially_copyable<T>::value, "Type must be POD");
@@ -20,6 +41,17 @@ bool pack(uint8_t* buffer, size_t buffer_len, uint8_t start_byte, T value) {
     return true;
 }
 
+/**
+ * @brief バッファからPOD型データを取り出す関数
+ *
+ * @tparam T 取り出すPOD型データの型
+ * @param buffer データを取り出すバッファ
+ * @param buffer_len バッファの長さ
+ * @param start_byte 開始バイト位置
+ * @param out_value 取り出したPOD型データの格納先
+ * @return true 成功
+ * @return false 失敗（バッファオーバーフローなど）
+ */
 template <typename T>
 bool unpack(const uint8_t* buffer, size_t buffer_len, uint8_t start_byte, T& out_value) {
     static_assert(std::is_trivially_copyable<T>::value, "Type must be POD");
@@ -31,6 +63,17 @@ bool unpack(const uint8_t* buffer, size_t buffer_len, uint8_t start_byte, T& out
     return true;
 }
 
+/**
+ * @brief std::arrayバッファにPOD型データを収納する関数
+ *
+ * @tparam T 収納するPOD型データの型
+ * @tparam N バッファのサイズ
+ * @param buffer データを収納するバッファ
+ * @param start_byte 開始バイト位置
+ * @param value 収納するPOD型データ
+ * @return true 成功
+ * @return false 失敗（バッファオーバーフローなど）
+ */
 template <typename T, size_t N>
 bool pack(std::array<uint8_t, N>& buffer, uint8_t start_byte, T value) {
     return pack(buffer.data(), N, start_byte, value);
