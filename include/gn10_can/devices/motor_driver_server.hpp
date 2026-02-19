bf660891f4b879b031549657dada855a16acbf11
aiba-gento <8gn24gn25@gmail.com>
2026-02-19 14:21:13 +0900

    Serverに必要な関数追加

--- include/gn10_can/devices/motor_driver_server.hpp
+++ include/gn10_can/devices/motor_driver_server.hpp
@@ -33,10 +33,10 @@ class MotorDriverServer : public CANDevice {
     /**
      * @brief モータードライバーフィードバック送信関数
      *
-     * @param current_val 現在値（速度制御の場合は速度、位置制御の場合は位置）
+     * @param feedback_val 現在値（速度制御の場合は速度、位置制御の場合は位置）
      * @param limit_switch_state リミットスイッチ状態（ビットマップ形式）
      */
-    void send_feedback(float current_val, uint8_t limit_switch_state);
+    void send_feedback(float feedback_val, uint8_t limit_switch_state);
 
     /**
      * @brief モータードライバー状態送信関数
@@ -44,7 +44,35 @@ class MotorDriverServer : public CANDevice {
      * @param load_current 電流
      * @param temperature 温度
      */
-    void send_status(float load_current, int8_t temperature);
+    void send_hardware_status(float load_current, int8_t temperature);
+
+    /**
+     * @brief 新しい設定があれば更新する
+     *
+     * @param config 設定
+     * @return true 新しい設定が有り更新した
+     * @return false 新しい設定はなく、更新しなかった
+     */
+    bool get_new_config(MotorConfig& config);
+
+    /**
+     * @brief 新しい目標値があれば更新する
+     *
+     * @param target モーター制御の目標値
+     * @return true 新しい目標値があり更新した
+     * @return false 新しい目標値はなく、更新しなかった
+     */
+    bool get_new_target(float& target);
+
+    /**
+     * @brief 新しいゲインがあれば更新する
+     *
+     * @param type ゲインの種類
+     * @param value ゲインの値
+     * @return true 新しいゲインがあり更新した
+     * @return false 新しいゲインはなく、更新しなかった
+     */
+    bool get_new_gain(GainType type, float& value);
 
     /**
      * @brief CANパケット受信時の呼び出し関数の実装
@@ -54,6 +82,14 @@ class MotorDriverServer : public CANDevice {
     void on_receive(const CANFrame& frame) override;
 
   private:
+    static constexpr std::size_t kGainTypeCount = static_cast<std::size_t>(GainType::Count);
+
+    MotorConfig config_;
+    bool is_config_updated_{false};
+    float target_{0.0f};
+    bool is_target_updated_{false};
+    float gains_[kGainTypeCount]{0.0f};
+    bool is_gains_updated_[kGainTypeCount]{false};
 };
 }  // namespace devices
 }  // namespace gn10_can
